1dfc96293ac1c88a346d39d234b88d2b174ac49f
afanda(范晓宁) <fan_apple1990@163.com>
2017-11-28 20:31:09 +0800

    release

--- build/CustomerSystem-ios.build/Release-iphoneos/CustomerSystem-ios.build/Objects-normal/arm64/FixFopen.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/FXN/环信移动客服/kefu-ios-demo/CustomerSystem-ios/CustomerSystem.pch \
-  /Users/FXN/环信移动客服/kefu-ios-demo/CustomerSystem-ios/HelpDeskUI/HDUIKit/Util/FixFopen.c
--- build/SharedPrecompiledHeaders/CustomerSystem-colidhzxilxvmoaxzyucdcrfpkdn/CustomerSystem.pch.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/FXN/环信移动客服/kefu-ios-demo/CustomerSystem-ios/CustomerSystem.pch
